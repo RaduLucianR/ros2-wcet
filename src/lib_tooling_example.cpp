5c83b7b4f2bb1f0ce74a0ca2566bed6a008e14ca
RaduLucianR <radlucradulescu@gmail.com>
2024-10-18 18:39:19 +0200

    add first clang working

---
+++ src/lib_tooling_example.cpp
@@ -0,0 +1,87 @@
+#include "clang/Driver/Options.h"
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/ASTConsumers.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace clang;
+using namespace clang::driver;
+using namespace clang::tooling;
+using namespace llvm;
+
+Rewriter rewriter;
+int numFunctions = 0;
+static cl::OptionCategory MyToolCategory("my-tool options");
+
+class ExampleVisitor : public RecursiveASTVisitor<ExampleVisitor> {
+private:
+    ASTContext *astContext; // used for getting additional AST info
+
+public:
+    explicit ExampleVisitor(CompilerInstance *CI) 
+      : astContext(&(CI->getASTContext())) // initialize private members
+    {
+        rewriter.setSourceMgr(astContext->getSourceManager(), astContext->getLangOpts());
+    }
+
+    virtual bool VisitFunctionDecl(FunctionDecl *func) {
+        numFunctions++;
+        string funcName = func->getNameInfo().getName().getAsString();
+        return true;
+    }
+};
+
+
+
+class ExampleASTConsumer : public ASTConsumer {
+private:
+    ExampleVisitor *visitor; // doesn't have to be private
+
+public:
+    // override the constructor in order to pass CI
+    explicit ExampleASTConsumer(CompilerInstance *CI)
+        : visitor(new ExampleVisitor(CI)) // initialize the visitor
+    { }
+
+    // override this to call our ExampleVisitor on the entire source file
+    virtual void HandleTranslationUnit(ASTContext &Context) {
+        /* we can use ASTContext to get the TranslationUnitDecl, which is
+             a single Decl that collectively represents the entire source file */
+        visitor->TraverseDecl(Context.getTranslationUnitDecl());
+    }
+};
+
+
+class ExampleFrontendAction : public ASTFrontendAction {
+public:
+    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef file) {
+        return std::make_unique<ExampleASTConsumer>(&CI); // pass CI pointer to ASTConsumer
+    }
+};
+
+
+int main(int argc, const char **argv) {
+    // parse the command-line args passed to your code
+    auto ExpectedParser = CommonOptionsParser::create(argc, argv, MyToolCategory);
+    if (!ExpectedParser) {
+        llvm::errs() << ExpectedParser.takeError();
+        return 1;
+    }
+    CommonOptionsParser& op = ExpectedParser.get();      
+    // create a new Clang Tool instance (a LibTooling environment)
+
+    ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+
+    int result = Tool.run(newFrontendActionFactory<ExampleFrontendAction>().get());
+
+    errs() << "\nFound " << numFunctions << " functions.\n\n";
+}
---
+++ src/test.cpp
@@ -0,0 +1,18 @@
+void fuck_yes()
+{
+    int a = 5;
+    a = a + 2;
+}
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int a = 3;
+    int b = 5;
+    int c = sum(a, b);
+    return c;
+}
