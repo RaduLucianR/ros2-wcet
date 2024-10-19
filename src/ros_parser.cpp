d24b0d4f534c71557a7df132c310615eac4c1fa4
RaduLucianR <radlucradulescu@gmail.com>
2024-10-19 17:17:59 +0200

    add dump node info to a json per source file

--- src/ros_parser.cpp
+++ src/ros_parser.cpp
@@ -5,8 +5,11 @@
 #include "clang/Tooling/Tooling.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/Basic/SourceManager.h"
 
 #include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 using namespace clang;
@@ -15,12 +18,9 @@ using namespace clang::tooling;
 using namespace llvm;
 using namespace clang::ast_matchers;
 
-int numFunctions = 0;
-int numVar = 0;
-int numClass = 0;
-int numConstr = 0;
-static cl::OptionCategory ROSParser("my-tool options");
-
+static cl::OptionCategory ROSParser("rosparser options");
+nlohmann::json jsonArray;
+std::string fileName = "";
 
 class NodeClassMatcher : public MatchFinder::MatchCallback {
 public:
@@ -28,8 +28,36 @@ public:
         if (const CXXRecordDecl *classDecl = Result.Nodes.getNodeAs<CXXRecordDecl>("derivedClass")) {
             std::cout << "Class: " << classDecl->getNameAsString() << std::endl;
 
+            /**
+             * ######## Get name of the file in which the Node Class resides ########
+             * TODO: Do this in a different way. It must be an easier way.
+             * Right now we check the location of each Node class 
+             * and we re-initialize the jsonArray if the current class 
+             * is in a new file.
+             * 
+             * Instead, we should already know *somehow* that we are 
+             * in a new file by this point, so the jsonArray should already
+             * correspond to the new file.
+             * 
+             * This portion of code is necessary because each file that
+             * contains Node Classes must have a JSON of its own.
+             */
+            SourceLocation loc = classDecl->getLocation();
+            const SourceManager &sourceManager = Result.Context->getSourceManager();     
+
+            if (fileName.compare(sourceManager.getFilename(loc).str()) != 0) {
+                fileName = sourceManager.getFilename(loc).str();
+                jsonArray = nlohmann::json::array();
+            }
+            // #######################################################################
+
+            nlohmann::json nodeClass;
+            nodeClass["name"] = classDecl->getNameAsString();
+            nlohmann::json callbacksArray = nlohmann::json::array();
+
             // Iterate over the declarations in the class
             for (const auto *decl : classDecl->decls()) {
+
                 // Check if the declaration is a member function
                 if (const auto *methodDecl = llvm::dyn_cast<CXXMethodDecl>(decl)) {
                     if (
@@ -39,17 +67,34 @@ public:
                         methodDecl->getNameAsString().rfind("operator", 0) > 0 // Check if member function is NOT operator
                     ) {       
                         std::cout << "  Public Member Function: " << methodDecl->getNameAsString() << endl;
+                        nlohmann::json callback;
+                        callback["name"] = methodDecl->getNameAsString();
+                        callback["param_count"] = 0;
+                        nlohmann::json parametersArray = nlohmann::json::array();
 
                         for (const auto *param : methodDecl->parameters()) {
-                            std::cout << "    " << param->getType().getAsString() << " "; // Get argument type
+                            nlohmann::json parameter;
+                            parameter["name"] = param->getNameAsString();
+                            parameter["type"] = param->getType().getAsString();
+                            parametersArray.push_back(parameter);
 
-                            if (!param->getNameAsString().empty()) {
-                                std::cout << param->getNameAsString() << endl; // Get argument name
-                            }
+                            std::cout << "    " << param->getType().getAsString() << " "; // Get argument type
+                            std::cout << param->getNameAsString() << endl;
                         }
+
+                        callback["parameters"] = parametersArray;
+                        callbacksArray.push_back(callback);
                     }
                 }
             }
+
+            nodeClass["callbacks"] = callbacksArray;
+            jsonArray.push_back(nodeClass);
+
+            // Write to file
+            std::string jsonFile = fileName + ".json";
+            ofstream Json(jsonFile);
+            Json << jsonArray.dump(4);
         }
     }
 };
@@ -57,6 +102,7 @@ public:
 int main(int argc, const char **argv) {
     // parse the command-line args passed to your code
     auto CliParser = CommonOptionsParser::create(argc, argv, ROSParser);
+
     if (!CliParser) {
         llvm::errs() << CliParser.takeError();
         return 1;
